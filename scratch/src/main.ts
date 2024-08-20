aa06f625569051bb34c30ae5ab28e9c9c2e2611b
devholic22 <hyunjoon.tech@gmail.com>
2024-08-20 22:27:46 +0900

    feat: bootstrap 및 AppModule 정의

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,4 +1,5 @@
-import { Controller, Module, Get } from "@nestjs/common";
+import { Controller, Module, Get } from "@nestjs/common"; // 필요한 임포트의 95%
+import { NestFactory } from "@nestjs/core"; // 드물게 사용
 
 @Controller() // 데코레이터: 애플리케이션 안에서 컨트롤러 역할을 할 클래스를 만든다고 명시
 class AppController {
@@ -7,3 +8,18 @@ class AppController {
         return 'hi there!';
     }
 }
+
+@Module({
+    // 설정 옵션 또는 객체를 넣어줌
+    controllers: [AppController] // 애플리케이션에 있는 모든 컨트롤러들을 나열 - Nest가 인스턴스들을 생성함. 스프링 빈과 유사
+})
+class AppModule {
+
+}
+
+async function bootstrap() { // 일종의 규약으로 bootstrap이라고 명시
+    const app = await NestFactory.create(AppModule); // Nest 애플리케이션 인스턴스
+    await app.listen(3000);
+}
+
+bootstrap();
