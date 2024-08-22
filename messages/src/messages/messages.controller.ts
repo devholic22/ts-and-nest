298fdc8993cea19677ef184a3a75184f6837dc5e
devholic22 <hyunjoon.tech@gmail.com>
2024-08-22 21:34:41 +0900

    feat: 인수 데코레이터 연결 작성

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,12 +8,12 @@ export class MessagesController {
     }
 
     @Post()
-    createMessages() {
-
+    createMessages(@Body() body: any) { // 유입되는 요청의 본문을 자동으로 추출하여 라우트 핸들러에 인수로 제공
+        console.log(body);
     }
 
     @Get('/:id')
-    getMessages() {
-
+    getMessages(@Param('id') id: string) {
+        console.log(id);
     }
 }
