697d1d1c848c166835ff2e99ae895d4b7459b8e8
devholic22 <hyunjoon.tech@gmail.com>
2024-08-27 01:04:19 +0900

    feat: 컨트롤러와 서비스 연결
    - 컨트롤러와 서비스 연결
    - messages.json 빈 객체 추가

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,20 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // 이것도 여기에서 직접 의존하고 있다. 실제 코드에서는 권장 X
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   // 기존 any에서 CreateMessageDto로 수정 반영
   createMessages(@Body() body: CreateMessageDto) {
     // 유입되는 요청의 본문을 자동으로 추출하여 라우트 핸들러에 인수로 제공
-    console.log(body);
+    this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessages(@Param('id') id: string) {
-    console.log(id);
+    this.messagesService.findOne(id);
   }
 }
