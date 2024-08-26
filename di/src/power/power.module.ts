19df9d80a7bf9ec1ce337b171a93fb9397c21d8e
devholic22 <hyunjoon.tech@gmail.com>
2024-08-27 01:39:00 +0900

    feat: 모듈 간 DI 실습

--- di/src/power/power.module.ts
+++ di/src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports: [PowerService], // 내보낼 클래스 정의
 })
 export class PowerModule {}
