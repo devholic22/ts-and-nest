b65728536aa8f41f9dedea33f6338786a36ef323
devholic22 <hyunjoon.tech@gmail.com>
2024-08-27 01:22:24 +0900

    docs: 주석 추가

--- messages/src/messages/messages.module.ts
+++ messages/src/messages/messages.module.ts
@@ -5,6 +5,6 @@ import { MessagesRepository } from './messages.repository';
 
 @Module({
   controllers: [MessagesController], // MessagesController 의존으로 변경됨 - 컨트롤러를 만들 때 모듈에 등록해야 하기 때문
-  providers: [MessagesService, MessagesRepository],
+  providers: [MessagesService, MessagesRepository], // 주입시켜주기
 })
 export class MessagesModule {}
